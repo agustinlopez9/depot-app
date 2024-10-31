b6dd981b84166207eca74b1f4a4d12709f8e17fd
agustinlopez9 <fernandopiceda99@gmail.com>
2024-10-31 02:28:49 -0300

    add cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -33,10 +33,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get edit_product_url(@product)
     assert_response :success
   end
-
   test "should update product" do
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
+  end
+
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
