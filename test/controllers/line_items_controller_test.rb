58c7f539cb670bc86203a04b998b2047412861c2
agustinlopez9 <fernandopiceda99@gmail.com>
2024-11-03 20:28:13 -0300

    fix tests

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -55,7 +55,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update line_item" do
     patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
-    assert_redirected_to line_item_url(@line_item)
+    assert_redirected_to store_index_url
   end
 
   test "should destroy line_item" do
@@ -63,6 +63,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       delete line_item_url(@line_item)
     end
 
-    assert_redirected_to cart_url
+    assert_redirected_to store_index_url
   end
 end
